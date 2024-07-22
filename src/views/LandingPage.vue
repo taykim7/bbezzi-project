4baa42ff4a58708e9eb77f2cdc23c1429d1e2478
taykim7 <th_77@naver.com>
2024-07-22 21:35:22 +0900

    chore: 랜딩화면 스타일 작업

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -1,12 +1,15 @@
 <template>
   <div class="landing">
-    <div class="bbezzi">bbezzi</div>
-    <div class="div">매일 체중을 기록하고,</div>
-    <div class="div2">시간에 따른 변화를 한눈에 확인하세요.</div>
-    <div class="rectangle-103"></div>
+    <div class="logo">bbezzi</div>
+    <div class="intro-text">
+      <p>매일 체중을 기록하고,<br />시간에 따른 변화를 한눈에 확인하세요.</p>
+    </div>
+    <!-- <div class="rectangle-103"></div>
     <div class="rectangle-104"></div>
     <div class="rectangle-106"></div>
-    <div class="rectangle-107"></div>
+    <div class="rectangle-107"></div> -->
+
+    <!-- TODO 버튼 컴포넌트화 -->
     <div class="component-1">
       <div class="group-42">
         <button class="rectangle-110" @click="moveLoginPage">로그인</button>
@@ -32,15 +35,29 @@ const moveLoginPage = () => {
 </script>
 
 <style scoped>
-.landing,
-.landing * {
-  box-sizing: border-box;
-}
 .landing {
-  background: #f2efe7;
-  height: 100%;
-  position: relative;
-  overflow: hidden;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+}
+.logo {
+  color: #343434;
+  text-align: center;
+  font-family: 'Pretendard-Black', sans-serif;
+  font-size: 60px;
+  font-weight: 900;
+
+  margin-top: 10rem;
+}
+.intro-text {
+  text-align: center;
+  font-family: var(--headline-m-font-family, 'Pretendard-Bold', sans-serif);
+  font-size: 2rem;
+  font-weight: var(--headline-m-font-weight, 700);
+  color: #343434;
+
+  margin-bottom: 10rem;
 }
 .component-1 {
   display: flex;
@@ -74,20 +91,6 @@ const moveLoginPage = () => {
   font-size: var(--button-l-font-size, 16px);
   font-weight: var(--button-l-font-weight, 700);
 }
-/* .btn-1 {
-  color: var(--gray4, #e8e8e8);
-  text-align: center;
-  font-family: var(--button-l-font-family, 'Pretendard-Bold', sans-serif);
-  font-size: var(--button-l-font-size, 16px);
-  font-weight: var(--button-l-font-weight, 700);
-  position: absolute;
-  right: 44.98%;
-  left: 44.98%;
-  width: 10.03%;
-  bottom: 54.12%;
-  top: 23.53%;
-  height: 22.35%;
-} */
 .btn-3 {
   color: var(--gray1, #343434);
   text-align: center;
@@ -95,45 +98,7 @@ const moveLoginPage = () => {
   font-size: var(--button-m-font-size, 14px);
   font-weight: var(--button-m-font-weight, 500);
 }
-.bbezzi {
-  color: #343434;
-  text-align: center;
-  font-family: 'Pretendard-Black', sans-serif;
-  font-size: 60px;
-  font-weight: 900;
-  position: absolute;
-  left: calc(50% - 98.5px);
-  top: 172px;
-}
-.group-34 {
-  position: absolute;
-  inset: 0;
-}
-.div {
-  color: #343434;
-  text-align: center;
-  font-family: var(--headline-m-font-family, 'Pretendard-Bold', sans-serif);
-  font-size: var(--headline-m-font-size, 22px);
-  font-weight: var(--headline-m-font-weight, 700);
-  position: absolute;
-  left: calc(50% - 94.5px);
-  top: 253px;
-}
-.div2 {
-  color: #343434;
-  text-align: center;
-  font-family: var(--headline-m-font-family, 'Pretendard-Bold', sans-serif);
-  font-size: var(--headline-m-font-size, 22px);
-  font-weight: var(--headline-m-font-weight, 700);
-  position: absolute;
-  left: calc(50% - 165.5px);
-  top: 284px;
-}
-.group-3 {
-  position: absolute;
-  inset: 0;
-}
-.rectangle-103 {
+/* .rectangle-103 {
   background: #d9d9d9;
   border-radius: 10px;
   width: 13px;
@@ -168,5 +133,5 @@ const moveLoginPage = () => {
   position: absolute;
   left: calc(50% - -31.5px);
   top: calc(50% - -174px);
-}
+} */
 </style>
