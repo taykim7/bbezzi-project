933682b735637e03f45b798ab145b6be4762aab3
taykim7 <th_77@naver.com>
2024-07-23 19:51:59 +0900

    style: 랜딩 페이지 버튼 적용

--- src/views/LandingPage.vue
+++ src/views/LandingPage.vue
@@ -9,12 +9,16 @@
     <div class="rectangle-106"></div>
     <div class="rectangle-107"></div> -->
 
-    <!-- TODO 버튼 컴포넌트화 -->
-    <div class="component-1">
-      <div class="group-42">
-        <button class="rectangle-110" @click="moveLoginPage">로그인</button>
+    <!-- 버튼 -->
+    <div class="btn-wrap">
+      <div class="btn-items">
+        <div class="btn-item">
+          <button class="btn-main" @click="moveLoginPage">로그인</button>
+          <div class="btn-one">
+            <button class="btn-center" @click="movePage">회원가입</button>
+          </div>
+        </div>
       </div>
-      <button class="btn-3" @click="movePage">회원가입</button>
     </div>
   </div>
 </template>
@@ -59,45 +63,7 @@ const moveLoginPage = () => {
 
   margin-bottom: 10rem;
 }
-.component-1 {
-  display: flex;
-  flex-direction: column;
-  gap: 10px;
-  align-items: center;
-  justify-content: flex-end;
-  width: 329px;
-  position: absolute;
-  left: calc(50% - 163.5px);
-  bottom: 101px;
-}
-.group-42 {
-  flex-shrink: 0;
-  width: 329px;
-  height: 58px;
-  position: static;
-}
-.rectangle-110 {
-  background: var(--gray1, #343434);
-  border-radius: 5px;
-  width: 329px;
-  height: 58px;
-  position: absolute;
-  left: calc(50% - 164.5px);
-  top: calc(50% - 42.5px);
 
-  color: var(--gray4, #e8e8e8);
-  text-align: center;
-  font-family: var(--button-l-font-family, 'Pretendard-Bold', sans-serif);
-  font-size: var(--button-l-font-size, 16px);
-  font-weight: var(--button-l-font-weight, 700);
-}
-.btn-3 {
-  color: var(--gray1, #343434);
-  text-align: center;
-  font-family: var(--button-m-font-family, 'Pretendard-Medium', sans-serif);
-  font-size: var(--button-m-font-size, 14px);
-  font-weight: var(--button-m-font-weight, 500);
-}
 /* .rectangle-103 {
   background: #d9d9d9;
   border-radius: 10px;
