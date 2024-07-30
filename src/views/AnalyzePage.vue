4c7aecaa6506d842c8140348fa4cd533388f373a
taykim7 <th_77@naver.com>
2024-07-30 19:15:00 +0900

    feat: 분석 화면

--- src/views/AnalyzePage.vue
+++ src/views/AnalyzePage.vue
@@ -41,11 +41,9 @@
 
     <div class="line"></div>
 
-    <!--  -->
-
     <div class="chart-wrap">
       <div class="mb40">
-        <div class="chart-title">기간 설정</div>
+        <div class="chart-title mb8">기간 설정</div>
         <div class="chart-btns">
           <button class="chart-btn-selected">1년</button>
           <button class="chart-btn">6개월</button>
@@ -63,8 +61,7 @@
 
     <div class="chart-wrap">
       <div class="mb40">
-        <div class="chart-title">업적 (1년)</div>
-
+        <div class="chart-title mb8">업적 (1년)</div>
         <div class="did-list">
           <div class="did-title mb8">320일의 체중을 기록하였습니다!</div>
           <div class="did-title">2.3 kg 감량했습니다!</div>
@@ -72,110 +69,43 @@
       </div>
     </div>
 
-    <div class="chart-wrap">
-      <div class="mb40">
-        <div class="chart-title">리스트</div>
-        <div class="_366">총 366 건</div>
-        <div class="did-list">
-          <div class="did-title mb8">320일의 체중을 기록하였습니다!</div>
-          <div class="did-title">2.3 kg 감량했습니다!</div>
+    <div class="list-wrap">
+      <div class="list-title mb8">리스트</div>
+      <div class="list-count mb8">총 366 건</div>
+      <div class="list-items">
+        <!-- 아이템1 -->
+        <div class="list-item">
+          <p class="list-date mr8">2024.07.01</p>
+          <p class="list-gram mr8">68.7</p>
+          <textarea class="list-memo mr8" readonly>배가 너무너무 고프다 오 ...</textarea>
+          <div class="list-btn-wrap">
+            <button class="list-edit mr8">
+              <img class="now-week-icon" src="../assets/logo.svg" height="19" />
+            </button>
+            <button class="list-delete">
+              <img class="now-week-icon" src="../assets/logo.svg" height="19" />
+            </button>
+          </div>
         </div>
-      </div>
-    </div>
-
-    <!--
-    <div class="group-63">
-      <div class="rectangle-27"></div>
-      <div class="_2024-07-01">2024.07.01</div>
-      <div class="_68-7">68.7</div>
-      <div class="div4">배가 너무너무 고프다 오 ...</div>
-      <div class="rectangle-29"></div>
-      <div class="rectangle-30"></div>
-    </div>
-    <div class="group-62">
-      <div class="rectangle-88"></div>
-      <div class="_2024-07-012">2024.07.01</div>
-      <div class="_68-72">68.7</div>
-      <div class="div5">
-        배가 너무너무 고프다 어쩌구저쩌구 배가 너무너무 고프다 어쩌구저쩌구 배가 너무너무 고프다
-        어쩌구저쩌구 배가 너무너무 고프다 어쩌구저쩌구
-      </div>
-      <div class="rectangle-89"></div>
-      <div class="rectangle-90"></div>
-    </div>
-    <div class="group-61">
-      <div class="rectangle-100"></div>
-      <div class="_2024-07-013">2024.07.01</div>
-      <div class="_68-73">68.7</div>
-      <div class="div6">배가 너무너무 고프다 오 ...</div>
-      <div class="rectangle-101"></div>
-      <div class="rectangle-102"></div>
-    </div> -->
-
-    <!-- 계산기 -->
-    <!-- <div class="weight-wrap">
-      <div class="weight-title">
-        <div class="weight-text">체중</div>
-        <div class="weight-date">2024-07-01 금요일</div>
-      </div>
-
-      <input class="weight-gram" value="68.5" />
-    </div> -->
-
-    <!-- <div class="input-btns">
-      <div class="input-btn-row">
-        <button class="input-btn-num">1</button>
-        <button class="input-btn-num">2</button>
-        <button class="input-btn-num">3</button>
-        <button class="input-btn-else">
-          <img class="input-btn-icon" src="../assets/logo.svg" height="19" />
-        </button>
-      </div>
-      <div class="input-btn-row">
-        <button class="input-btn-num">4</button>
-        <button class="input-btn-num">5</button>
-        <button class="input-btn-num">6</button>
-        <button class="input-btn-else">
-          <img class="input-btn-icon" src="../assets/logo.svg" height="19" />
-        </button>
-      </div>
-      <div class="input-btn-row">
-        <button class="input-btn-num">7</button>
-        <button class="input-btn-num">8</button>
-        <button class="input-btn-num">9</button>
-        <button class="input-btn-else">
-          <img class="input-btn-icon" src="../assets/logo.svg" height="19" />
-        </button>
-      </div>
-    </div> -->
-
-    <!-- TODO 메모 textarea -->
-    <!-- <div class="input-wrap">
-      <div class="input-items">
-        <div class="mb40">
-          <div class="input-title">메모</div>
-          <div class="input-contents">
-            <textarea class="input-textarea" placeholder="메모를 입력하세요" rows="1"></textarea>
+        <!-- 아이템2 -->
+        <div class="list-item-detail">
+          <p class="list-date mr8">2024.07.01</p>
+          <p class="list-gram mr8">68.7</p>
+          <textarea class="list-memo mr8">배가 너무너무 고프다 오 ...</textarea>
+          <div class="list-btn-wrap">
+            <button class="list-edit mr8">
+              <img class="now-week-icon" src="../assets/logo.svg" height="19" />
+            </button>
+            <button class="list-delete">
+              <img class="now-week-icon" src="../assets/logo.svg" height="19" />
+            </button>
           </div>
         </div>
       </div>
-    </div> -->
-
-    <!-- TODO 저장하기 버튼 -->
-    <!-- 버튼 -->
-    <!-- <div class="btn-wrap">
-      <div class="btn-items">
-        <div class="btn-item">
-          <div class="btn-two">
-            <button class="btn-main-left" @click="movePage">수정하기</button>
-            <button class="btn-main-right" @click="movePage">삭제하기</button>
-          </div>
-        </div>
-      </div>
-    </div> -->
+    </div>
   </div>
 
-  <!-- <div class="bnb">
+  <div class="bnb">
     <div class="bnb-btns">
       <div class="bnb-home">
         <img class="bnb-icon" src="../assets/logo.svg" height="19" />
@@ -186,7 +116,7 @@
         <button class="bnb-btn" @click="movePage">분석</button>
       </div>
     </div>
-  </div> -->
+  </div>
 </template>
 
 <script setup>
@@ -422,8 +352,6 @@ const movePage = () => {
   width: 100%;
 }
 
-/* TODO  */
-
 .chart-wrap {
   width: 100%;
   margin-top: 2rem;
@@ -433,8 +361,6 @@ const movePage = () => {
   text-align: left;
   font-size: 20px;
   font-weight: 600;
-
-  margin-bottom: 10px;
 }
 
 .chart-btns {
@@ -508,627 +434,88 @@ const movePage = () => {
   font-weight: 400;
 }
 
-.rectangle-77 {
-  background: #343434;
-  width: 421px;
-  height: 139px;
-  position: absolute;
-  left: calc(50% - 210.5px);
-  top: 1061px;
+.list-wrap {
+  width: 100%;
 }
-.group-73 {
-  position: absolute;
-  inset: 0;
-}
-.rectangle-2 {
-  background: #343434;
-  border-radius: 10px;
-  width: 34px;
-  height: 19px;
-  position: absolute;
-  left: 225px;
-  top: 104px;
-}
-.ellipse-1 {
-  background: #fff9eb;
-  border-radius: 50%;
-  width: 45px;
-  height: 44px;
-  position: absolute;
-  left: 40px;
-  top: 132px;
-}
-.ellipse-2 {
-  background: #343434;
-  border-radius: 50%;
-  width: 45px;
-  height: 44px;
-  position: absolute;
-  left: 85px;
-  top: 132px;
-}
-.ellipse-3 {
-  background: #343434;
-  border-radius: 50%;
-  width: 45px;
-  height: 44px;
-  position: absolute;
-  left: 130px;
-  top: 132px;
-}
-.ellipse-4 {
-  background: #fff9eb;
-  border-radius: 50%;
-  width: 44px;
-  height: 44px;
-  position: absolute;
-  left: 175px;
-  top: 132px;
-}
-.ellipse-6 {
-  background: #fff9eb;
-  border-radius: 50%;
-  width: 45px;
-  height: 44px;
-  position: absolute;
-  left: 264px;
-  top: 132px;
-}
-.ellipse-7 {
-  background: #fff9eb;
-  border-radius: 50%;
-  width: 45px;
-  height: 44px;
-  position: absolute;
-  left: 309px;
-  top: 132px;
-}
-._28 {
-  color: #1e1e1e;
-  text-align: center;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 54px;
-  top: 145px;
-}
-.mon {
-  color: #343434;
-  text-align: center;
-  font-family: 'NixgonfontsV20-M', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 47px;
-  top: 106px;
-}
-.group-2 {
-  width: 98px;
-  height: 24px;
-  position: static;
-}
-._2024 {
+.list-wrap .list-title {
   color: #343434;
   text-align: left;
-  font-family: 'Pretendard-Regular', sans-serif;
-  font-size: 20px;
-  font-weight: 400;
-  position: absolute;
-  left: 148px;
-  top: 60px;
-}
-._7 {
-  color: #343434;
-  text-align: left;
-  font-family: 'Pretendard-ExtraBold', sans-serif;
-  font-size: 20px;
-  font-weight: 800;
-  position: absolute;
-  left: 217px;
-  top: 60px;
-}
-._29 {
-  color: #f2efe7;
-  text-align: center;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 99px;
-  top: 145px;
-}
-.tue {
-  color: #343434;
-  text-align: center;
-  font-family: 'NixgonfontsV20-M', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 95px;
-  top: 106px;
-}
-._30 {
-  color: #f2efe7;
-  text-align: center;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 144px;
-  top: 145px;
-}
-.wed {
-  color: #343434;
-  text-align: center;
-  font-family: 'NixgonfontsV20-M', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 137px;
-  top: 106px;
-}
-._31 {
-  color: #1e1e1e;
-  text-align: center;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 189px;
-  top: 145px;
-}
-.thu {
-  color: #343434;
-  text-align: center;
-  font-family: 'NixgonfontsV20-M', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 185px;
-  top: 106px;
-}
-._2 {
-  color: #1e1e1e;
-  text-align: center;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 282px;
-  top: 145px;
-}
-.sat {
-  color: #343434;
-  text-align: center;
-  font-family: 'NixgonfontsV20-M', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 276px;
-  top: 106px;
-}
-._3 {
-  color: #1e1e1e;
-  text-align: center;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 327px;
-  top: 145px;
-}
-.sun {
-  color: #343434;
-  text-align: center;
-  font-family: 'NixgonfontsV20-M', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 323px;
-  top: 106px;
-}
-.ellipse-5 {
-  background: #343434;
-  border-radius: 50%;
-  border-style: solid;
-  border-color: #343434;
-  border-width: 2px;
-  width: 45px;
-  height: 44px;
-  position: absolute;
-  left: 219px;
-  top: 131px;
-}
-._1 {
-  color: #f2efe7;
-  text-align: center;
-  font-family: 'Freesentation-9Black', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 237px;
-  top: 144px;
-}
-.fri {
-  color: #f2efe7;
-  text-align: center;
-  font-family: 'NixgonfontsV20-B', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 232px;
-  top: 106px;
-}
-.vector-2 {
-  width: 389.5px;
-  height: 0px;
-  position: absolute;
-  left: calc(50% - 194.5px);
-  top: 204px;
-  overflow: visible;
-}
-.group-1 {
-  width: 91.33%;
-  height: 1.49%;
-  position: absolute;
-  right: 4.36%;
-  left: 4.31%;
-  bottom: 86.47%;
-  top: 12.04%;
-  overflow: visible;
-}
-.vector {
-  width: 3.82%;
-  height: 1.42%;
-  position: absolute;
-  right: 64.38%;
-  left: 31.81%;
-  bottom: 93.33%;
-  top: 5.25%;
-  overflow: visible;
-}
-.div {
-  color: #848383;
-  text-align: center;
-  font-family: 'Freesentation-5Medium', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 82px;
-  top: 1110px;
-}
-.div2 {
-  color: #f2efe7;
-  text-align: center;
-  font-family: 'Freesentation-5Medium', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 292px;
-  top: 1110px;
-}
-.vector2 {
-  width: 4.58%;
-  height: 1.67%;
-  position: absolute;
-  right: 75.32%;
-  left: 20.1%;
-  bottom: 8.33%;
-  top: 90%;
-  overflow: visible;
-}
-.vector3 {
-  width: 4.58%;
-  height: 1.67%;
-  position: absolute;
-  right: 20.36%;
-  left: 75.06%;
-  bottom: 8.33%;
-  top: 90%;
-  overflow: visible;
-}
-.group-64 {
-  position: absolute;
-  inset: 0;
-}
-.div3 {
-  color: #343434;
-  text-align: left;
-  font-family: 'Pretendard-SemiBold', sans-serif;
   font-size: 20px;
   font-weight: 600;
-  position: absolute;
-  left: 32px;
-  top: 753px;
 }
-
-.group-63 {
-  width: 329px;
-  height: 39px;
-  position: static;
-}
-.rectangle-27 {
-  background: #343434;
-  border-radius: 5px;
-  width: 329px;
-  height: 39px;
-  position: absolute;
-  left: calc(50% - 164.5px);
-  top: 811px;
-}
-._2024-07-01 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-2ExtraLight', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 45px;
-  top: 822px;
-}
-._68-7 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-9Black', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 133px;
-  top: 822px;
-}
-.div4 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 179px;
-  top: 823px;
-}
-.rectangle-29 {
-  background: #706f6f;
-  border-radius: 10px;
-  width: 16px;
-  height: 15px;
-  position: absolute;
-  left: 312px;
-  top: 823px;
-}
-.rectangle-30 {
-  background: #706f6f;
-  border-radius: 10px;
-  width: 16px;
-  height: 15px;
-  position: absolute;
-  left: 333px;
-  top: 823px;
-}
-.group-62 {
-  width: 329px;
-  height: 129px;
-  position: static;
-}
-.rectangle-88 {
-  background: #343434;
-  border-radius: 5px;
-  width: 329px;
-  height: 129px;
-  position: absolute;
-  left: calc(50% - 164.5px);
-  top: 860px;
-}
-._2024-07-012 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-2ExtraLight', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 45px;
-  top: 871px;
-}
-._68-72 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-9Black', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 133px;
-  top: 871px;
-}
-.div5 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 179px;
-  top: 872px;
-  width: 130px;
-  height: 105px;
-}
-.rectangle-89 {
-  background: #706f6f;
-  border-radius: 10px;
-  width: 16px;
-  height: 15px;
-  position: absolute;
-  left: 312px;
-  top: 872px;
-}
-.rectangle-90 {
-  background: #706f6f;
-  border-radius: 10px;
-  width: 16px;
-  height: 15px;
-  position: absolute;
-  left: 333px;
-  top: 872px;
-}
-.group-61 {
-  width: 329px;
-  height: 39px;
-  position: static;
-}
-.rectangle-100 {
-  background: #343434;
-  border-radius: 5px;
-  width: 329px;
-  height: 39px;
-  position: absolute;
-  left: calc(50% - 164.5px);
-  top: 999px;
-}
-._2024-07-013 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-2ExtraLight', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 45px;
-  top: 1010px;
-}
-._68-73 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-9Black', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 133px;
-  top: 1010px;
-}
-.div6 {
-  color: #f2efe7;
-  text-align: left;
-  font-family: 'Freesentation-4Regular', sans-serif;
-  font-size: 15px;
-  font-weight: 400;
-  position: absolute;
-  left: 179px;
-  top: 1011px;
-}
-.rectangle-101 {
-  background: #706f6f;
-  border-radius: 10px;
-  width: 16px;
-  height: 15px;
-  position: absolute;
-  left: 312px;
-  top: 1011px;
-}
-.rectangle-102 {
-  background: #706f6f;
-  border-radius: 10px;
-  width: 16px;
-  height: 15px;
-  position: absolute;
-  left: 333px;
-  top: 1011px;
-}
-.group-72 {
-  position: absolute;
-  inset: 0;
-}
-.div7 {
+.list-count {
   color: #343434;
   text-align: left;
-  font-family: 'Pretendard-Bold', sans-serif;
-  font-size: 20px;
-  font-weight: 700;
-  position: absolute;
-  left: calc(50% - 163.5px);
-  top: 230px;
+  font-size: 15px;
+  font-weight: 400;
 }
-.group-68 {
-  width: 70px;
-  height: 28px;
-  position: static;
+.list-items {
+  width: 100%;
+  display: flex;
+  flex-direction: column;
 }
-
-.group-69 {
-  width: 70px;
-  height: 28px;
-  position: static;
-}
-
-.group-71 {
-  width: 70px;
-  height: 28px;
-  position: static;
-}
-.rectangle-81 {
-  background: #fff9eb;
+.list-item {
+  background-color: #343434;
   border-radius: 5px;
-  width: 70px;
-  height: 28px;
-  position: absolute;
-  left: 291px;
-  top: 265px;
+  height: 39px;
+  align-items: top;
+  margin-bottom: 0.8rem;
+  padding: 10px;
+  display: flex;
+  justify-content: space-between;
 }
-
-.group-70 {
-  width: 70px;
-  height: 28px;
-  position: static;
-}
-.rectangle-80 {
-  background: #fff9eb;
-  border-radius: 5px;
-  width: 70px;
-  height: 28px;
-  position: absolute;
-  left: 205px;
-  top: 265px;
-}
-
-.group-60 {
-  position: absolute;
-  inset: 0;
-}
-
-.group-67 {
-  position: absolute;
-  inset: 0;
-}
-._15 {
-  color: #343434;
+.list-date {
+  color: #f2efe7;
   text-align: left;
-  font-family: 'Pretendard-SemiBold', sans-serif;
-  font-size: 20px;
-  font-weight: 600;
-  position: absolute;
-  left: 32px;
-  top: 604px;
+  font-size: 15px;
+  font-weight: 400;
+  align-items: center;
+  justify-content: center;
 }
-
-.group-65 {
-  width: 329px;
-  height: 39px;
-  position: static;
+.list-gram {
+  color: #f2efe7;
+  text-align: left;
+  font-size: 15px;
+  font-weight: 400;
 }
-.rectangle-104 {
-  background: #343434;
+.list-item-detail {
+  background-color: #343434;
   border-radius: 5px;
-  width: 329px;
-  height: 39px;
-  position: absolute;
-  left: calc(50% - 164.5px);
-  top: 686px;
+  height: 129px;
+  display: flex;
+  align-items: top;
+  margin-bottom: 0.8rem;
+  padding: 10px;
+
+  justify-content: space-between;
 }
-._2-3-kg {
-  color: var(--background, #f2efe7);
-  text-align: center;
-  font-family: 'Pretendard-Bold', sans-serif;
-  font-size: 20px;
-  font-weight: 700;
-  position: absolute;
-  left: calc(50% - 86.5px);
-  top: 694px;
+.list-item .list-memo {
+  background: transparent;
+  width: 50%;
+  color: #f2efe7;
+  text-align: left;
+  font-size: 15px;
+  font-weight: 400;
+}
+.list-item-detail .list-memo {
+  background: transparent;
+  width: 50%;
+  height: 100%;
+  color: #f2efe7;
+  text-align: left;
+  font-size: 15px;
+  font-weight: 400;
+}
+.list-edit {
+  background: #706f6f;
+  border-radius: 10px;
+  width: 16px;
+  height: 15px;
+}
+.list-delete {
+  background: #706f6f;
+  border-radius: 10px;
+  width: 16px;
+  height: 15px;
 }
 </style>
