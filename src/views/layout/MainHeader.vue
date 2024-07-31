fa8e50b264b7f5d6f438b0179dda908497e48389
taykim7 <th_77@naver.com>
2024-07-31 23:14:52 +0900

    feat: 공통 헤더 제작

---
+++ src/views/layout/MainHeader.vue
@@ -0,0 +1,60 @@
+<template>
+  <div v-if="showBack || showLogout" class="header">
+    <div v-if="showBack" class="header-back">
+      <img @click="moveBackPage" class="back-btn" src="../assets/logo.svg" />
+    </div>
+    <div v-if="showLogout" class="header-logout">
+      <button class="logout-btn" @click="moveLandingPage">로그아웃</button>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { useRoute, useRouter } from 'vue-router'
+import { computed } from 'vue'
+
+const route = useRoute()
+const router = useRouter()
+
+const showBack = computed(() => {
+  return route.meta.back
+})
+const showLogout = computed(() => {
+  return route.meta.logout
+})
+
+// 뒤로가기
+const moveBackPage = () => {
+  router.back()
+}
+
+// 로그아웃
+const moveLandingPage = () => {
+  router.push({
+    path: '/'
+  })
+}
+</script>
+
+<style scoped>
+/* 뒤로가기 */
+.header .header-back {
+  position: fixed;
+  top: 3rem;
+  left: 3rem;
+}
+/* .header .header-back .back-btn {
+} */
+/* 로그아웃 */
+.header .header-logout {
+  position: fixed;
+  top: 3rem;
+  right: 3rem;
+}
+.header .header-logout .logout-btn {
+  color: #848383;
+  font-size: 15px;
+  font-weight: 400;
+  width: 100%;
+}
+</style>
