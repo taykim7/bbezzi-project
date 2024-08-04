6a4adbecbec6e5e63ba896aa222d68156cd4ab28
taykim7 <th_77@naver.com>
2024-08-04 20:42:22 +0900

    feat: 공통 input 컴포넌트 제작

---
+++ src/components/CommonInput.vue
@@ -0,0 +1,85 @@
+<template>
+  <div class="input-title">{{ title || 'title' }}</div>
+  <div v-if="type !== 'textarea'" class="input-contents">
+    <input
+      class="input-item"
+      :type="type || 'text'"
+      :placeholder="placeholder || 'placeholder'"
+      :value="modelValue || ''"
+      @input="$emit('update:modelValue', $event.target.value)"
+    />
+    <!-- 중복확인 -->
+    <button v-if="doubleCheck" class="input-btn">중복확인</button>
+  </div>
+  <div v-else class="input-contents">
+    <!-- textarea -->
+    <textarea
+      class="input-textarea"
+      :placeholder="placeholder || 'placeholder'"
+      :value="modelValue || ''"
+      rows="1"
+    ></textarea>
+  </div>
+</template>
+<script setup>
+defineEmits(['update:modelValue'])
+defineProps({
+  modelValue: String,
+  type: String,
+  placeholder: String,
+  title: String,
+  doubleCheck: Boolean
+})
+</script>
+<style>
+.input-title {
+  color: #343434;
+  text-align: left;
+  font-size: 20px;
+  font-weight: 600;
+}
+.input-contents {
+  display: flex;
+  justify-content: space-between;
+}
+.input-wrap .input-contents .input-btn {
+  color: #ff5353;
+  font-size: 15px;
+  font-weight: 300;
+  border-radius: 30px;
+  border-style: solid;
+  border-color: #ff5353;
+  border-width: 1px;
+  width: 130px;
+  height: 35px;
+}
+.input-contents .input-item {
+  color: #343434;
+  text-align: left;
+  font-size: 15px;
+  font-weight: 600;
+
+  border-width: 0 0 1px;
+  background: transparent;
+  width: 100%;
+  padding: 5px 0 5px 0;
+}
+.input-contents .input-item::placeholder {
+  color: #bababa;
+}
+/* textarea */
+.input-contents .input-textarea {
+  color: #343434;
+  text-align: left;
+  font-size: 15px;
+  font-weight: 600;
+
+  border-width: 0 0 1px;
+  background: transparent;
+  width: 100%;
+  padding: 5px 0 5px 0;
+}
+.input-contents .input-textarea::placeholder {
+  color: #bababa;
+}
+</style>
--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import './css/base.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
+import CommonInput from './components/CommonInput.vue'
 
 const app = createApp(App)
 
 // 등록
 app.use(router)
+
+// global components
+app.component('C-Input', CommonInput)
 
 app.mount('#app')
