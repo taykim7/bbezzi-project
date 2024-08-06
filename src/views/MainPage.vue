563fe6ac7a458f72c3d515b19738b7bd01d7fb07
taykim7 <th_77@naver.com>
2024-08-06 20:21:21 +0900

    feat: 한 주의 날짜를 표시
    - 맨 처음은 '오늘'을 기준
    - 날짜 조절시 해당 날짜를 기준으로 한 주가 표시된다.

--- src/views/MainPage.vue
+++ src/views/MainPage.vue
@@ -2,40 +2,24 @@
   <div class="main">
     <div class="now-date mb16">
       <img class="now-date-icon mr8" src="../assets/img/svg/icon_calendar.svg" height="25" />
-      <div class="now-year mr8"><p>2024년</p></div>
-      <div class="now-month"><p>7월</p></div>
+      <div class="now-year mr8">
+        <p>{{ displayDate.getFullYear() }}년</p>
+      </div>
+      <div class="now-month">
+        <p>{{ displayDate.getMonth() + 1 + '월' }}</p>
+      </div>
     </div>
 
     <div class="now-week">
       <img class="now-week-icon" src="../assets/img/svg/icon_before.svg" height="25" />
-      <div class="now-week-one">
-        <div class="now-week-day">Mon</div>
-        <button class="now-week-date">28</button>
-      </div>
-      <div class="now-week-one">
-        <div class="now-week-day">Tue</div>
-        <button class="now-week-date">28</button>
-      </div>
-      <div class="now-week-one">
-        <div class="now-week-day">Wed</div>
-        <button class="now-week-date">28</button>
-      </div>
-      <div class="now-week-one">
-        <div class="now-week-day">Thu</div>
-        <button class="now-week-date">28</button>
-      </div>
-      <div class="now-week-one">
-        <div class="now-week-day">Fri</div>
-        <button class="now-week-date">28</button>
-      </div>
-      <div class="now-week-one">
-        <div class="now-week-day">Sat</div>
-        <button class="now-week-date">28</button>
-      </div>
-      <div class="now-week-one">
-        <div class="now-week-day">Sun</div>
-        <button class="now-week-date">28</button>
-      </div>
+      <template v-for="(item, index) in displaydays" :key="index">
+        <div class="now-week-one">
+          <div class="now-week-day" :class="today.getDay() === index ? 'today' : ''">
+            {{ dayArr[index] }}
+          </div>
+          <button class="now-week-date">{{ item }}</button>
+        </div>
+      </template>
       <img class="now-week-icon" src="../assets/img/svg/icon_after.svg" height="25" />
     </div>
 
@@ -45,7 +29,27 @@
   </div>
 </template>
 
-<script setup></script>
+<script setup>
+const dayArr = ['Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat', 'Sun']
+
+const today = new Date()
+console.log(today)
+
+/**
+ * 표시할 한 주의 날짜 계산
+ * - 처음 기준은 '오늘'
+ * - 날짜를 이동하면 해당 날짜 기준으로 세팅이 된다.
+ */
+const displayDate = new Date()
+const displaydays = []
+function setDisplayWeek() {
+  for (let i = 0; i < 7; i++) {
+    displaydays.push(displayDate.getDate() - displayDate.getDay() + i)
+  }
+}
+
+setDisplayWeek()
+</script>
 
 <style scoped>
 .main {
@@ -87,30 +91,39 @@
   flex-direction: column;
 }
 .now-week-day {
-  color: #f2efe7;
-  background-color: #343434;
-
+  color: #343434;
   width: 40px;
   border-radius: 20px;
-
   text-align: center;
   font-size: 15px;
   font-weight: 400;
-
   margin-bottom: 0.4rem;
 }
 .now-week-date {
-  /* background-color: none; */
-  /* background-color: #fff9eb; */
+  color: #343434;
+  width: 45px;
+  height: 45px;
+  border-radius: 50%;
+  text-align: center;
+  font-size: 15px;
+  font-weight: 400;
+}
+.now-week-day.today {
+  color: #f2efe7;
+  background-color: #343434;
+  border-radius: 20px;
+  font-size: 15px;
+  font-weight: 600;
+}
+.now-week-date.did {
   color: #f2efe7;
   background-color: #343434;
   width: 45px;
   height: 45px;
   border-radius: 50%;
-
   text-align: center;
   font-size: 15px;
-  font-weight: 400;
+  font-weight: 600;
 }
 .line {
   width: 100%;
