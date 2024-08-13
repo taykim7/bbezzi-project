8d394f9ee6cfe74addd76f8e9a49e7c6810640ef
taykim7 <th_77@naver.com>
2024-08-13 19:29:21 +0900

    feat: 임시 차트 생성

--- src/views/analyze/AnalyzeChartView.vue
+++ src/views/analyze/AnalyzeChartView.vue
@@ -1,8 +1,31 @@
 <template>
-  <div class="chart-space"></div>
+  <div class="chart-space">
+    <V-Line :data="data" :options="options" />
+  </div>
+  <button @click="test">클릭</button>
 </template>
 
-<script setup></script>
+<script setup>
+import { ref } from 'vue'
+
+// 옵션
+const options = {
+  responsive: true,
+  maintainAspectRatio: false
+}
+
+// 데이터
+const data = ref({
+  labels: ['240101', '240101', '240101', '240101', '240101', '240101', '240101'],
+  datasets: [
+    {
+      label: 'Data One',
+      backgroundColor: '#f87979',
+      data: [60, 61, 63, 65, 68, 64, 61]
+    }
+  ]
+})
+</script>
 
 <style scoped>
 .chart-space {
