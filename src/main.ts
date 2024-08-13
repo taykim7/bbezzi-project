cd702c40a55b69ca1de762a15eab6a32ac446e10
taykim7 <th_77@naver.com>
2024-08-13 19:29:11 +0900

    feat: vue-chart 등록

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,18 @@ import App from './App.vue'
 import router from './routes'
 import CommonInput from './components/CommonInput.vue'
 import CommonButton from './components/CommonButton.vue'
+import { Line } from 'vue-chartjs'
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+} from 'chart.js'
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Title, Tooltip, Legend)
 
 const app = createApp(App)
 
@@ -14,5 +26,6 @@ app.use(router)
 // global components
 app.component('C-Input', CommonInput)
 app.component('C-Button', CommonButton)
+app.component('V-Line', Line)
 
 app.mount('#app')
