caedd81672137fb6b215d30e07661e713a44f687
taykim7 <th_77@naver.com>
2024-08-23 20:52:25 +0900

    feat: firebase 연동
    - firebaseConfig 분리
    - firebase auth, db 추출

---
+++ src/config/firebaseConfig.js
@@ -0,0 +1,11 @@
+const firebaseConfig = {
+  apiKey: 'AIzaSyD3T1yNQ90bhH9UrKHiR7ez6hIOWyia7b4',
+  authDomain: 'taylab-bc1ab.firebaseapp.com',
+  databaseURL: 'https://taylab-bc1ab-default-rtdb.asia-southeast1.firebasedatabase.app',
+  projectId: 'taylab-bc1ab',
+  storageBucket: 'taylab-bc1ab.appspot.com',
+  messagingSenderId: '948414322886',
+  appId: '1:948414322886:web:dc24d9d4c3c5cad0b91fc7',
+  measurementId: 'G-X38ETBVM40'
+}
+export { firebaseConfig }
