729f9157ff909dd6c56cd8b800d6cffc99a2b2db
taykim7 <th_77@naver.com>
2024-09-03 19:57:18 +0900

    feat: post 범위 데이터를 가져올 수 있도록 수정

--- src/stores/post.js
+++ src/stores/post.js
@@ -1,16 +1,21 @@
 import { defineStore } from 'pinia'
 import { db } from '@/plugins/firebase'
-import { query, collection, orderBy, getDocs } from 'firebase/firestore'
+import { query, collection, orderBy, getDocs, where } from 'firebase/firestore'
 
 export const usePostStore = defineStore('post', {
   state: () => ({
     posts: []
   }),
   actions: {
-    async fetchPosts(uid) {
+    async fetchPosts(uid, start, end) {
       try {
         const url = `post/${uid}/datas`
-        const q = await query(collection(db, url), orderBy('standardDate', 'desc'))
+        const q = await query(
+          collection(db, url),
+          where('standardDate', '>=', start),
+          where('standardDate', '<=', end),
+          orderBy('standardDate', 'desc')
+        )
         this.posts = []
         const response = await getDocs(q)
         response.forEach((doc) => {
