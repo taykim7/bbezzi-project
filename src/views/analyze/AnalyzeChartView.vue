40232c0de64476e37c7c47acd88cc6e2f0f35cbb
taykim7 <th_77@naver.com>
2024-09-25 21:41:44 +0900

    style: chart 커스텀 진행 중

--- src/views/analyze/AnalyzeChartView.vue
+++ src/views/analyze/AnalyzeChartView.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="chart-space">
-    <V-Line :data="data" :options="options" />
+    <V-Line :data="data" :options="options" style="background-color: #f2ebe7" />
   </div>
 </template>
 
@@ -11,10 +11,21 @@ const props = defineProps({
   posts: Object
 })
 
+// TODO 커스텀툴팁
 // 옵션
 const options = {
   responsive: true,
-  maintainAspectRatio: false
+  maintainAspectRatio: false,
+  scales: {
+    x: {
+      display: false
+    }
+  },
+  plugins: {
+    legend: {
+      display: false
+    }
+  }
 }
 
 // 데이터
@@ -22,13 +33,31 @@ const data = ref({
   labels: [],
   datasets: [
     {
-      label: 'Data One',
-      backgroundColor: '#f87979',
+      label: '체중',
+      backgroundColor: '#343434',
+      borderJoinStyle: 'round',
       data: []
     }
   ]
 })
 
+// 데이터 수에 따라 크기 변경
+const postsCount = props.posts.length
+if (postsCount <= 7) {
+  data.value.datasets[0].pointRadius = 7
+  data.value.datasets[0].borderWidth = 7
+} else if (7 < postsCount && postsCount <= 20) {
+  data.value.datasets[0].pointRadius = 5
+  data.value.datasets[0].borderWidth = 5
+} else if (20 < postsCount && postsCount <= 180) {
+  data.value.datasets[0].pointRadius = 3
+  data.value.datasets[0].borderWidth = 3
+} else {
+  data.value.datasets[0].pointRadius = 1
+  data.value.datasets[0].borderWidth = 1
+}
+
+// 데이터 삽입
 for (const element of props.posts) {
   data.value.labels.push(element.standardDate)
   data.value.datasets[0].data.push(element.gram)
