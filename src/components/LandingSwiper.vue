dad84996e93d7df42c59becabe14b53961440441
taykim7 <th_77@naver.com>
2024-09-28 17:07:41 +0900

    style: swiper 수정
    - 테스트 이미지 등록
    - 스와이퍼 페이지 bullet 수정
    - 스와이퍼 페이지네이션 위치 변경

--- src/components/LandingSwiper.vue
+++ src/components/LandingSwiper.vue
@@ -7,10 +7,10 @@
     :modules="modules"
     class="mySwiper"
   >
-    <swiper-slide>Slide 1</swiper-slide>
-    <swiper-slide>Slide 2</swiper-slide>
-    <swiper-slide>Slide 3</swiper-slide>
-    <swiper-slide>Slide 4</swiper-slide>
+    <swiper-slide><img src="@/assets/img/img/test1.png" alt="test" /></swiper-slide>
+    <swiper-slide><img src="@/assets/img/img/test1.png" alt="test" /></swiper-slide>
+    <swiper-slide><img src="@/assets/img/img/test1.png" alt="test" /></swiper-slide>
+    <swiper-slide><img src="@/assets/img/img/test1.png" alt="test" /></swiper-slide>
   </swiper>
 </template>
 
@@ -24,20 +24,18 @@ const modules = [Pagination]
 <style>
 .swiper {
   width: 100%;
-  height: 100%;
 }
 
 .swiper-slide {
   text-align: center;
   font-size: 18px;
   background-color: #d9d9d9;
-  height: 35rem;
   /* Center slide text vertically */
   display: flex;
   justify-content: center;
   align-items: center;
+  margin-bottom: 0.8rem;
 }
-
 .swiper-slide img {
   display: block;
   width: 100%;
@@ -45,15 +43,20 @@ const modules = [Pagination]
   object-fit: cover;
 }
 .swiper-pagination .swiper-pagination-bullet {
-  background: #ffffff;
+  background: #343434;
   border-radius: 10px;
-  width: 13px;
-  height: 13px;
+  width: 10px;
+  height: 10px;
 }
 .swiper-pagination .swiper-pagination-bullet-active {
   background: #343434;
   border-radius: 10px;
-  width: 32px;
-  height: 13px;
+  width: 30px;
+  height: 10px;
+}
+.swiper-pagination {
+  display: block;
+  position: relative;
+  bottom: 0% !important;
 }
 </style>
